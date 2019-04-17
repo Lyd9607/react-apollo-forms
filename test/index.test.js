1bbb4929847f209a7bdc733c2009cedece7d0f7d
Barry <s@zhso.net>
2019-04-17 07:51:12 -0700

    ✅

--- test/index.test.js
+++ test/index.test.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import { ApolloProvider } from "react-apollo";
-import {shallow, mount, render} from 'enzyme';
-import { JSDOM } from 'jsdom';
+import { mount } from 'enzyme';
 import gql from 'graphql-tag';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { withClientState } from 'apollo-link-state';
 import { ApolloLink } from 'apollo-link';
-
 import { Button } from '../lib';
 
 const client = new ApolloClient({
-    link: ApolloLink.from([       
+    link: ApolloLink.from([
         withClientState({
             defaults: {},
         }),
     ]),
-    cache: new InMemoryCache({ })
+    cache: new InMemoryCache({})
 });
 
 const mutation = gql`
@@ -26,11 +24,6 @@ mutation($name: String!){
     }
 }
 `;
-
-const { window } = new JSDOM('<!doctype html><html><body></body></html>');
-
-global.window = window;
-global.document = window.document;
 
 describe('Mount Button', function () {
     it('Mount Button Children', function () {
