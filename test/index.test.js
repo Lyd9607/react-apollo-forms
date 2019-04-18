1ab1732715e86194d71570becb3f25930da59991
Barry <s@zhso.net>
2019-04-17 23:48:14 -0700

    ✅

--- test/index.test.js
+++ test/index.test.js
@@ -12,29 +12,58 @@ const client = new ApolloClient({
     link: ApolloLink.from([
         withClientState({
             defaults: {},
+            resolvers: {
+                Query: {
+                    getName: (_, variables, { cache }) => {
+                        const name = cache.readFragment({ fragment: gql`
+                            fragment Hero @client{
+                            name
+                            }
+                        ` });
+
+                        return name;
+                    },
+                },
+                Mutation: {
+                    updateName: (_, { name }, { cache }) => {
+                        cache.writeData({
+                          data: {
+                            name
+                          },
+                        });
+
+                        return name;
+                    }
+                },
+            },
         }),
     ]),
     cache: new InMemoryCache({})
 });
 
 const mutation = gql`
-mutation($name: String!){
-    updateName(name: $name){
-        name
-    }
+mutation($name: String!) {
+    updateName(name: $name) @client
 }
 `;
 
 describe('Mount Button', function () {
     it('Mount Button Children', function () {
-        let button = mount(
+        const button = mount(
             <ApolloProvider client={client}>
-                <Button mutation={mutation}>
+                <Button
+                    mutation={mutation}
+                    variables={{ name: 'Oliver Queen' }}
+                    onCompleted={({ updateName })=>{
+                        expect(updateName).toEqual('Oliver Queen');
+                    }}
+                >
                     Barry Allen
                 </Button>
             </ApolloProvider>
         );
 
         expect(button.find('button').text()).toEqual('Barry Allen');
+        button.simulate('click');
     });
 });
